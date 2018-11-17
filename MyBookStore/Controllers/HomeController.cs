bbcb55499624f36c1a03273b5833856a38ec096a
Elena <elena.shynkarenko@gmail.com>
2018-11-17 05:03:41 +0200

    Added models, controller and view

--- MyBookStore/Controllers/HomeController.cs
+++ MyBookStore/Controllers/HomeController.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyBookStore.Models;
 
 namespace MyBookStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
             return View();
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Buy(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.BookId = id;
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public string Buy(Purchase purchase)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
         }
     }
 }
--- MyBookStore/Models/Book.cs
+++ MyBookStore/Models/Book.cs
@@ -7,5 +7,9 @@ namespace MyBookStore.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
     }
 }
