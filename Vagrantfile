39afb930d9817ca8b8b3e970c7d8ddeb8e78e24b
Elena <elena.shynkarenko@gmail.com>
2018-12-04 19:46:11 +0200

    updates

--- Vagrantfile
+++ Vagrantfile
@@ -5,12 +5,12 @@ Vagrant.configure(2) do |config|
   config.vm.boot_timeout = 300
   config.vm.network :forwarded_port, guest: 3389, host: 3389
   config.vm.network :forwarded_port, guest: 5985, host: 5985, id: "winrm", auto_correct: true
-  #config.vm.network :public_network
+  config.vm.network :public_network
+  config.vm.synced_folder ".", "/vagrant"
 
   config.vm.provider "virtualbox" do |vb|
     vb.gui = true
     vb.memory = 1024
-    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
   end
 
   config.vm.provision "shell", path: "iis_setup.ps1", privileged: true
--- iis_setup.ps1
+++ iis_setup.ps1
@@ -1,35 +1,44 @@
 ﻿Set-ExecutionPolicy Bypass -Scope Process -Force 
-#iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#Install-WindowsFeature -name Web-Server -IncludeManagementTools
-choco install git.install
-choco install visualstudio2017-workload-webbuildtools
-choco install visualstudio2017buildtools
-choco install nuget.commandline
-choco install nuget.commandline
 Import-Module WebAdministration
-
 $IISSiteName = "BookStore"
+$RepoFolderPath = "C:\BookStoreRepo" 
 $SiteFolderPath = "C:\BookStore"
-$AppPoolName = "BookStoreAppPool"          
+$AppPoolName = "BookStoreAppPool" 
 $HostHeader = "www.BookStore.com"
 $GitHubUrl = "https://github.com/LenaShy/MyBookStore.git" 
- 
 
-if(!(Test-Path("$SiteFolderPath"))) {
-    New-Item $SiteFolderPath -type Directory
-    Set-Content $SiteFolderPath\Default.htm "<h1>Hello IIS</h1>"
+
+
+if(!(Test-Path("$RepoFolderPath"))) { 
+New-Item $RepoFolderPath -type Directory # creating folder with repo
+Set-Location -Path $RepoFolderPath # go to location where repo is sited
+git clone $GitHubUrl
+
+} 
+else # if folder exist
+{
+Set-Location -Path $RepoFolderPath # go to location where repo is sited
+git pull
 }
 
-if(!(Test-Path ("IIS:\AppPools\$AppPoolName"))) {
-    New-WebAppPool -Name $AppPoolName
+Set-Location -Path "$RepoFolderPath\MyBookStore" # go to inner folder in cloned repo
+msbuild # build 
+
+
+$SiteFolderPath = "$RepoFolderPath\MyBookStore\MyBookStore"
+
+if(!(Test-Path ("IIS:\AppPools\$AppPoolName"))) { # if not AppPool -> create it
+New-WebAppPool -Name $AppPoolName
 }
 
-If((!(Test-Path "IIS:\Sites\$IISSiteName"))){
-    New-Website -Name $IISSiteName -physicalPath $SiteFolderPath  -ApplicationPool $AppPoolName        
+If((!(Test-Path "IIS:\Sites\$IISSiteName"))){ # if not site -> create it
+New-Website -Name $IISSiteName -physicalPath $SiteFolderPath -ApplicationPool $AppPoolName 
 }
 
 if(!(Get-WebBinding -Name $IISSiteName)) {
-    New-WebBinding -Name $IISSiteName -Protocol http -Port 80 -IPAddress "*" -HostHeader $HostHeader
+New-WebBinding -Name $IISSiteName -Protocol http -Port 80 -IPAddress "*" -HostHeader $HostHeader
 }
-
+if(Get-Website | where {$_.State -eq 'Stopped'})
+{
 Start-WebSite -Name $IISSiteName
+}
