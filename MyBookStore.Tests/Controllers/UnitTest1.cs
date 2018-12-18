dc2e92a99155ff774e70873a08a7c9478e3a62bd
Olena Shynkarenko <oshynk@softserveinc.com>
2018-12-18 17:11:19 +0200

    added simple controller tests

---
+++ MyBookStore.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyBookStore;
+using MyBookStore.Controllers;
+
+namespace MyBookStore.Tests.Controllers
+{
+    [TestClass]
+    public class StoreControllerTest
+    {
+        [TestMethod]
+        public void IndexViewResultNotNull()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.ToTest() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexViewEqualIndexCshtml()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.ToTest() as ViewResult;
+
+            Assert.AreEqual("ToTest", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexStringInViewbag()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.ToTest() as ViewResult;
+
+            Assert.AreEqual("Hello world!", result.ViewBag.Message);
+        }
+    }
+}
